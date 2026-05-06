217e3f096e3641bddbec528416b2de4c2a8c7cdb
KamaNila <mkaramoko16@myges.fr>
2026-05-06 15:27:52 +0200

    exo1 et exo2

--- src/exo/Partie1.java
+++ src/exo/Partie1.java
@@ -2,22 +2,41 @@ package exo;
 
 import models.Trip;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Partie1 {
 
+    Predicate<Trip> longEtCher = trip -> trip.distanceKm() > 10 && trip.price() > 20;
+
+    Predicate<Trip> mauvaiseNote = trip -> trip.rating() < 3;
+
+    Predicate<Trip> trajetsRecents = trip -> {
+        LocalDate aujourdhui = LocalDate.now();
+        LocalDate dateTrajets = trip.startTime().toLocalDate();
+        return dateTrajets.equals(aujourdhui) || dateTrajets.equals(aujourdhui.minusDays(1));
+    };
+
+    //Exercice 1
     public List<Trip> longAndExpensiveTrips(List<Trip> trips) {
-        // distance > 10km et prix > 20€
-        return List.of();
+        return trips.stream()
+                .filter(longEtCher)
+                .collect(Collectors.toList());
     }
 
+    //Exercice 2
     public List<Trip> badTrips(List<Trip> trips) {
-        // rating < 3
-        return List.of();
+        return trips.stream()
+                .filter(mauvaiseNote)
+                .collect(Collectors.toList());
     }
 
+    //Exercice 3
     public List<Trip> recentTrips(List<Trip> trips) {
-        // aujourd’hui ou hier
-        return List.of();
+        return trips.stream()
+                .filter(trajetsRecents)
+                .collect(Collectors.toList());
     }
 }
--- src/exo/Partie2.java
+++ src/exo/Partie2.java
@@ -4,23 +4,28 @@ import models.Trip;
 
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Partie2 {
 
+    Function<Trip, String> parVille     = Trip::city;
+    Function<Trip, String> parChauffeur = Trip::driverId;
+
     public Map<String, Long> countByCity(List<Trip> trips) {
-        // coder ici
-        return Map.of();
+        return trips.stream()
+                .collect(Collectors.groupingBy(parVille, Collectors.counting()));
     }
 
     public Map<String, Double> revenueByDriver(List<Trip> trips) {
-        // revenu par chauffeur
-        // coder ici
-        return Map.of();
+        return trips.stream()
+                .collect(Collectors.groupingBy(parChauffeur,
+                        Collectors.summingDouble(Trip::price)));
     }
 
     public Map<String, Double> avgDurationByCity(List<Trip> trips) {
-        // coder ici
-        // durée moyenne par ville
-        return Map.of();
+        return trips.stream()
+                .collect(Collectors.groupingBy(parVille,
+                        Collectors.averagingDouble(Trip::durationMin)));
     }
 }
